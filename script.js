a0354f4f12e1f93305531d90f0bf088e9e4cf90d
dlbrptrvc <37641298+dlbrptrvc@users.noreply.github.com>
2023-10-26 19:53:25 +0200

    Started working from inside out. Awaiting json

--- script.js
+++ script.js
@@ -1,58 +1,51 @@
-function initializeDOM(parentElementID) {
-    window[parentElementID] = document.querySelector('#'+parentElementID)
-    Array.from(window[parentElementID].children).forEach(element => {
-        if (element.id) {initializeDOM(element.id)}        
-    });
-}
-initializeDOM('body')
+class Book {
+    constructor(title,author,published,acquired,location) {
+        this.title = title || 'Unknown'
+        this.author = author || 'Unknown'
+        this.published = published || 'Unknown'
+        this.acquired = acquired || new Date().getFullYear().toString()
+        this.location = location || 'Home'
+    }
 
-let myLib = []
-
-function Book(title,author,read) {
-    this.title = title
-    this.author = author
-    this.read = read
-    this.info = function() {
-        let readmsg = "Read id."
-        if (read==false) {readmsg = "Haven't read it"}
-        return (this.title +' by '+this.author+". "+readmsg)
+    info() {
+        return (this.title +' by '+this.author+". Pub:"+this.published+" Acq:"+this.acquired+" Currently at "+this.location)
     }
 }
 
-function addBookToLibrary(title,author,read) {
-    let newBook = new Book(title,author,read)
-    myLib.push(newBook)
-    populateList()
+let MyLib = {
+    num : 0,
+    books : {},
+    findBook: function(words,searchBy) {
+        let results = []
+        if (searchBy!=='all') {
+            searchBy == searchBy || 'title'
+            Object.entries(this.books).forEach((entry)=>{
+                if (entry[1][searchBy].includes(words)) {
+                    results.push(entry[0])
+                }
+            })
+        } else {
+            Object.getOwnPropertyNames(new Book).forEach((property)=>{
+                searchBy = property
+                Object.entries(this.books).forEach((entry)=>{
+                    if (entry[1][searchBy].includes(words)) {
+                        results.push(entry[0])
+                    }
+                })
+            })
+        }
+        return results
+    },
+    addBook: function(book){
+        MyLib.books[this.num]=book
+        this.num++
+    },
+    removeBook: function(id){
+        delete MyLib.books[id]
+    },
+    editBook: function(id){
+        return MyLib.books[id]
+    }
 }
 
-addBookToLibrary("Последња Књига","Зоран Живковић",false)
-addBookToLibrary("Tropic of Cancer","Henry Miller",true)
-addBookToLibrary("1984","George Orwell",true)
-
-
-function populateList() {
-    Array.from(libList.children).forEach((item)=>{
-        item.remove()
-    })
-    myLib.forEach( (item,index)=>{
-        let row = document.createElement('div')
-        row.className = 'row'
-        row.textContent = (index+1).toString() +". "+item.info()
-        let removeButton = document.createElement('button')
-        removeButton.textContent = "Remove"
-        removeButton.addEventListener('click', ()=> {
-            myLib.splice(index,1)
-            populateList()
-        })
-        row.append(removeButton)
-        libList.append(row)
-    })
-}
-
-populateList()
-
-addButton.addEventListener('click',(event)=>{
-    event.preventDefault()
-    addBookToLibrary(title.value,author.value,read.value)
-    populateList()
-})
+MyLib.addBook(new Book("1984","George Orwell"))
